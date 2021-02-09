f7bfdfc817027dd733653163c268ec8bbd81a468
MartijnStoorvogel <martijnstoorvogel@gmail.com>
2021-02-09 14:39:57 +0100

    mail

--- app/templates/sendemail.php
+++ app/templates/sendemail.php
@@ -1,16 +1,15 @@
 <?php
-       // from the form
-       if (isset($_POST['submit'])){
-              $name = $_POST['name'];
-              $subject = $_POST['subject'];
-              $mail = $_POST['mail'];
-              $message = $_POST['message'];
+// from the form
+if (isset($_POST['submit'])){
+       $name = $_POST['name'];
+       $subject = $_POST['subject'];
+       $mail = $_POST['mail'];
+       $message = $_POST['message'];
 
-              $mailto = "machytech@yahoo.com";
-              $headers = "From: ".$mail;
-              $text = "You have received an e-mail from ".$name.".\n\n".$message
+       $mailto = "machytech@yahoo.com";
+       $headers = "From: ".$mail;
+       $text = "You have received an e-mail from ".$name.".\n\n".$message
 
-              mail($mailTo, $subject, $txt, $headers);
-              header("Location: template.html?mailsend")
-       }
-?>
+       mail($mailTo, $subject, $txt, $headers);
+       header("Location: template.html?mailsend")
+}
