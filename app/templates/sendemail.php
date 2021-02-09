e083045e20e1b46013f00625f9d9f3b2973ffa75
MartijnStoorvogel <martijnstoorvogel@gmail.com>
2021-02-09 14:24:05 +0100

    mail

--- app/templates/sendemail.php
+++ app/templates/sendemail.php
@@ -1,23 +1,16 @@
 <?php
        // from the form
-       $name = trim(strip_tags($_POST['name']));
-       $email = trim(strip_tags($_POST['email']));
-       $message = htmlentities($_POST['message']);
+       if (isset($_POST['submit'])){
+              $name = $_POST['name'];
+              $subject = $_POST['subject'];
+              $mail = $_POST['mail'];
+              $message = $_POST['message'];
 
-       // set here
-       $subject = "Contact form submitted!";
-       $to = 'machytech@yahoo.com';
+              $mailto = "machytech@yahoo.com";
+              $headers = "From: ".$mail;
+              $text = "You have received an e-mail from ".$name.".\n\n".$message
 
-       $body = <<<HTML
-$message
-HTML;
-
-       $headers = "From: $email\r\n";
-       $headers .= "Content-type: text/html\r\n";
-
-       // send the email
-       mail($to, $subject, $body, $headers);
-
-       // redirect afterwords, if needed
-       header('Location: thanks.html');
+              mail($mailTo, $subject, $txt, $headers);
+              header("Location: template.html?mailsend")
+       }
 ?>
